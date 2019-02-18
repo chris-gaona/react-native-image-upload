f878dcdf15e6aa80ed11df33ebd4aecb348fc011
Chris Gaona <chrisgaona@Chriss-STS-MacBook-Pro.local>
2019-02-18 14:04:17 -0700

    Writes function to create FormData for uploading image

--- MobileImageUpload/App.js
+++ MobileImageUpload/App.js
@@ -16,6 +16,22 @@ export default class App extends Component {
     photo: null,
   }
 
+  createFormData = (photo, body) => {
+    const data = new FormData()
+
+    data.append('photo', {
+      name: photo.fileName,
+      type: photo.type,
+      uri: Platform.OS === "android" ? photo.uri : photo.uri.replace("file://", "")
+    })
+
+    Object.keys(body).forEach(key => {
+      data.append(key, body[key]);
+    })
+
+    return data
+  }
+
   handleChoosePhoto = () => {
     const options = {
       title: 'Select Avatar',
